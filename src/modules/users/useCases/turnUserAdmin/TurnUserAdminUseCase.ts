bcfbc804cb4150c80635cb45b66ae422af37c0a1
MicaelRiboura <micaelribouradossantos@gmail.com>
2022-03-01 09:52:39 -0300

    fix(TurnUserAdminUseCase): add validation of user found

--- src/modules/users/useCases/turnUserAdmin/TurnUserAdminUseCase.ts
+++ src/modules/users/useCases/turnUserAdmin/TurnUserAdminUseCase.ts
@@ -10,6 +10,9 @@ class TurnUserAdminUseCase {
 
   execute({ user_id }: IRequest): User {
     const user = this.usersRepository.findById(user_id);
+    if (!user) {
+      throw new Error("user not found");
+    }
     const updatedUser = this.usersRepository.turnAdmin(user);
 
     return updatedUser;
